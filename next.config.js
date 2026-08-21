249a13d2103640b2815355f5a8f334c56c4f4889
anzarv <anzarvsharif@gmail.com>
2026-08-21 10:44:55 +0600

    Update next.config.js for static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your standard config options
+  output: 'export',
+  basePath: '/entro-website',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
